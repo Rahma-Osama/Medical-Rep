07480b5675d376e427b2df033f47d485064ea109
Rahma <rahmaosama2804@gmail.com>
2026-03-16 23:20:57 +0200

    add app theme

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,37 @@
+import 'package:finance_app/core/styling/app_colors.dart';
+import 'package:finance_app/core/styling/app_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextStyle{
+  static TextStyle appName = TextStyle(
+    fontSize: 35.sp,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle HeadLine1 = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle title = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.bold,
+    color: AppColors.blackColor,
+  );
+
+  static TextStyle subtitle = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.blackColor,
+  );
+
+  static TextStyle body = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.blackColor,
+  );
+
+}
