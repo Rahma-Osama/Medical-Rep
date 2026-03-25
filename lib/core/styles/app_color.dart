fb2c31e42f142d872e19ad67baafbf82d0ae4d0a
Doaa-15 <doaakhamis69@gmail.com>
2026-03-25 21:06:34 +0200

    style edit

--- lib/core/styles/app_color.dart
+++ lib/core/styles/app_color.dart
@@ -5,6 +5,7 @@ class AppColors {
   static Color thirdColor = Color(0xff456882);
   static Color backgroundColor = Color(0xffE3E3E3);
   static Color blackColor = Color(0xff1F2C37);
-  static Color grayColor = Color(0xff9CA4AB);
+  static Color grayColor = Color.fromARGB(255, 95, 96, 96);
+   static Color lightgrayColor = Color.fromARGB(255, 230, 233, 233);
   static Color whiteColor = Color(0xffffffff);
 }
