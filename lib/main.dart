26dfa7a25b4b55673eda1d02567e6e89074fe2e9
manarmahmoud81 <101602825+manarmahmoud81@users.noreply.github.com>
2026-03-30 18:52:48 +0200

    Finished doctor and pharmacy ui.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:medical_rep/features/doctor_and_pharmacy/views/entities_list_page.dart';
+import 'package:medical_rep/features/doctor_and_pharmacy/views/pharmacy_profile_page.dart';
 import 'package:medical_rep/features/weekly_planning/views/create_weekly_plan_view.dart';
 import 'package:medical_rep/features/weekly_planning/views/weekly_plan_status_view.dart';
 
@@ -12,7 +14,7 @@ class MedicalApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WeeklyPlanningView(),
+      home:WeeklyPlanningView(),
     );
   }
 }
