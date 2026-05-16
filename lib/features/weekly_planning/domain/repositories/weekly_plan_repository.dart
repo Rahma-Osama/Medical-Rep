c1a5aea8ec349421d803779d3b0e8218bbf2475b
Doaa-15 <doaakhamis69@gmail.com>
2026-05-16 19:42:49 +0300

    final edit

--- lib/features/weekly_planning/domain/repositories/weekly_plan_repository.dart
+++ lib/features/weekly_planning/domain/repositories/weekly_plan_repository.dart
@@ -2,7 +2,7 @@ import 'package:medical_rep/features/weekly_planning/domain/entities/visit_entit
 
 abstract class WeeklyPlanRepository {
   Future<void> saveWeeklyPlan(Map<int, List<VisitEntity>> weeklyData);
-// جوه ملف الـ Abstract Class (weekly_plan_repository.dart) عدلي السطر ده:
+
 Future<Map<int, List<VisitEntity>>> getLocalPlan();
   Future<void> submitFullPlan();
   
